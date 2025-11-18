49e145407cb216c8fb7244cda28c99930d47aa41
fernandoOS03 <fernando.192023@outlook.es>
2025-11-18 07:18:09 -0500

    refactor : Correción del login

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -1,4 +1,4 @@
-import { createContext, useState, useEffect } from "react";
+import { createContext, useState } from "react";
 
 import axiosInstance from "../api/axiosInstance.js";
 
--- src/main.jsx
+++ src/main.jsx
@@ -1,18 +1,16 @@
-import React from 'react'
-import { createRoot } from 'react-dom/client'
-import { AppRouter } from './router/AppRouter.jsx'
-import { AuthProvider } from './context/AuthContext.jsx'
-import App from './App.jsx';
+import React from 'react';
+import { createRoot } from 'react-dom/client'; 
+import { AppRouter } from './routes/AppRouter.jsx'; 
+import { AuthProvider } from './context/AuthContext.jsx'; 
+import './index.css'; 
 
-
-//import './index.css'
-//import App from './App.jsx'
-
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <AuthProvider>
-      <AppRouter/>
-    </AuthProvider>
-  </React.StrictMode>
+// El código se monta aquí
+createRoot(document.getElementById('root')).render(
+    <React.StrictMode>
+        {/* AuthProvider envuelve todo para dar acceso al contexto */}
+        <AuthProvider> 
+            {/* AppRouter maneja la navegación (incluyendo /login) */}
+            <AppRouter />
+        </AuthProvider>
+    </React.StrictMode>
 );
